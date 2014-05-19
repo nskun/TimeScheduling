454ce0263f3e67a6f70d165b1afe60c6ed45afb8
nskun <8086.nk@gmail.com>
2014-05-20 07:21:32 +0900

    Revert "createAdd"
    This reverts commit 6df5c285e9379aa7b842f58e2b9f79a782f38c0b.

--- InputData.h
+++
@@ -1,5 +0,0 @@
-#ifndef INPUTDATA_H
-#define INPUTDATA_H
-
-void InputData(int **Se, int *Es, int **Rf, int *Fr, int **Ef, int *Fe, int *roomsize, int *event, int *room, int *feature, int *student, int *attendance);
-#endif
--- Kswap.h
+++
@@ -1,4 +0,0 @@
-#ifndef KSWAP_H
-#define KSWAP_H
-void RoomTimeslotKswap(int **event_timeslot, int **room_timeslot, int **student_timeslot, int *room, int *event, int *student, int **event_student, int **student_event, int *student_hp, int **roomfeature_event, int **room_event);
-#endif
